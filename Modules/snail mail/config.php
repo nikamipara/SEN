71ac332959bcfc7d3ec5eb088e19adc09351e956
Nikunj Amipara <nikamipara29@gmail.com>
2013-04-08 22:20:03 +0530

    email added

--- Modules/snail mail/config.php
+++ Modules/snail mail/config.php
@@ -2,5 +2,5 @@
 	define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'nikunj');
-    define('DB_DATABASE', 'hms');
+    define('DB_DATABASE', 'sen');
 ?>
--- Modules/snail mail/smenter.php
+++ Modules/snail mail/smenter.php
@@ -1,11 +1,61 @@
+
+             
 <?php
+
+//include ('sen/databasefun.php');
+/*session_start();
+if(!isset($_SESSION['access']) or $_SESSION['access']!= 4)
+{   
+	$_SESSION['access']=0;
+	session_destroy();
+	header('location:/sen/Modules/login.php');
+}*/
 
 require("phpmailer/class.phpmailer.php");
 require("phpmailer/class.smtp.php");
+require_once("db.php");
+
+$db=Connect_To_Server();
+$db_found=Connect_To_DB();	
+$date1 = new DateTime();
+$date = $date1->format('Y-m-d');
+$time = $date1->format('H:i:s');
+
+$userid= $_POST['id'];
+$sentby1 = $_POST['sentby'];
+//echo $date . $time . $userid . $sentby1 . gettype($_POST['sentby']); 
+
+$query1= "INSERT INTO `snail_mail`(`date`, `time`, `id`, `sentby`) VALUES (NOW() , NOW(), $userid , '' '.$sentby1.' '') ";
+//$query1="INSERT INTO `snail_mail`(`date`, `time`, `id`, `sentby`) VALUES (2013-03-27 , '06:50:50' , '201001199' ,'spped postr') ";
+
+$result = mysql_query($query1);
+				if($result==false)
+				{
+						echo 'some thing went wrong !!!!!!!!'. mysql_error();
+				}
+				else
+				{	
+					echo 'snail mail update added successfully for '. $userid;
+					createmail( $userid , $sentby1);
+				}
+
+//5. close connection
+
+mysql_close($db);
+
+
+
+
+?>
+<?php
+
 function createmail( $userid , $sentby)
 {	
 require_once("db.php");
-	$mail = new PHPMailer(); 
+
+$result3 = mysql_query("SELECT  t2.email FROM  (select * from login where login_id = '$userid') as t1  LEFT JOIN residents as t2 on (t1.login_id = t2.login_id ) ");
+$row1 = mysql_fetch_array($result3);
+$mail = new PHPMailer(); 
 $mail->IsSMTP();
 $mail->SMTPAuth = true; // turn on SMTP authentication
 $mail->Username = "daiicthor@gmail.com"; // SMTP username
@@ -39,7 +89,7 @@ $mail->SMTPKeepAlive = true;                  // SMTP connection will not close 
 
 
 	
-				$mail->AddAddress($post['email'],$userid);
+				$mail->AddAddress($row1['email'],$userid);
 				
 				
 	
@@ -55,53 +105,5 @@ $mail->SMTPKeepAlive = true;                  // SMTP connection will not close 
 }
 
 	 
-
-		?>
-        
-        
-        
-<?php
-
-//include ('sen/databasefun.php');
-
-session_start();
-require_once("db.php");
-if(isset($_SESSION['access'])&&($_SESSION['access']=='4'))
-{
-$db=Connect_To_Server();
-$db_found=Connect_To_DB();	
-$date1 = new DateTime();
-$date = $date1->format('Y-m-d');
-$time = $date1->format('H:i:s');
-
-$userid= $_POST['id'];
-$sentby1 = $_POST['sentby'];
-$emailid;
-//echo $date . $time . $userid . $sentby1 . gettype($_POST['sentby']); 
-
-$query1= "INSERT INTO `snail_mail`(`date`, `time`, `id`, `sentby`) VALUES (NOW() , NOW(), $userid , '' '.$sentby1.' '') ";
-//$query1="INSERT INTO `snail_mail`(`date`, `time`, `id`, `sentby`) VALUES (2013-03-27 , '06:50:50' , '201001199' ,'spped postr') ";
-
-$result = mysql_query($query1);
-				if($result==false)
-				{
-						echo 'some thing went wrong !!!!!!!!'. mysql_error();
-				}
-				else
-				{	
-					echo 'snail mail update added successfully for '. $userid;
-				}
-
-//5. close connection
-
-mysql_close($db);
-}
-else 
-		{
-			$_SESSION['access']=0;
-			session_destroy();
-			header('location:/sen/Modules/login.php');
-			echo "invalid Login";
-		}
 
 ?>
