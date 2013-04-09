89ff7f5f1c383c4911c8f54bbb688a47fe39135e
Nikunj Amipara <nikamipara29@gmail.com>
2013-04-09 17:26:54 +0530

    hmc view for admin added

---
+++ Modules/admin/db.php
@@ -0,0 +1,23 @@
+<?php
+function Connect_TO_Server()
+		{
+			$usernamedb="root";
+			$passworddb="";
+			$server=$_SERVER['SERVER_ADDR'];
+			$db_handle=mysql_connect($server,$usernamedb,$passworddb);
+			return $db_handle; 
+		}
+		function Connect_TO_DB()
+		{
+			$database="sen";
+			$db_found = mysql_select_db($database);
+			if(!$db_found)
+			{
+				print "error in connection to database";
+			}
+		}
+		function Close_To_Server($db_handle)
+		{
+			mysql_close($db_handle);
+		}
+?>
